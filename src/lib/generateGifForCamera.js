2a3926fd1807127841d978bcb5c53bdb8c82d8fa
Bryce Meyer <bjm904@gmail.com>
2021-04-14 10:31:57 -0700

    Use ffmpeg to generate final GIF

--- src/lib/generateGifForCamera.js
+++ src/lib/generateGifForCamera.js
@@ -1,7 +1,6 @@
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const fs = require('fs');
-const GIFEncoder = require('gifencoder'); 
 const path = require('path');
-const pngFileStream = require('png-file-stream');
 const spawn = require('child_process').spawn;
 
 const gifPerams = {
@@ -12,20 +11,72 @@ const gifPerams = {
 
 const generateGifForCamera = (node, tmpDirectory, camera) => new Promise((resolve, reject) => {
   const tmpCamDirectory = path.join(tmpDirectory, camera);
-  const inputFilesPath = path.join(tmpCamDirectory, '*.png');
+  const inputFilesPath = path.join(tmpCamDirectory, '%d.png');
+  const outputVideoPath = path.join(tmpCamDirectory, 'video.avi');
   const outputPath = path.join(tmpCamDirectory, 'output.gif');
   
-  node.debug(`Creating gif ${outputPath}`);
+  node.debug(`Creating video ${outputVideoPath}`);
 
-  //TODO: Pull dimensions from sample file
-  const encoder = new GIFEncoder(1920, 1080);
+  const ffmpegVideoArgs = [
+    '-hide_banner',
+    '-i', inputFilesPath,
+    outputVideoPath,
+  ];
 
-  const stream = pngFileStream(inputFilesPath);
-  stream.pipe(encoder.createWriteStream(gifPerams))
-  stream.pipe(fs.createWriteStream(outputPath));
+  const ffmpegVideo = spawn(ffmpegPath, ffmpegVideoArgs);
 
-  stream.on('finish', () => {
-    resolve();
+  ffmpegVideo.on('error', (err) => {
+    node.warn(err);
+  });
+
+  if (false) {
+    ffmpegVideo.stdout.on('data', (data) => {
+      node.warn(`${data}`);
+    });
+
+    ffmpegVideo.stderr.on('data', (data) => {
+      node.warn(`${data}`);
+    });
+  }
+
+  ffmpegVideo.on('exit', (code) => {
+    if (code === 0) {
+      node.debug(`Creating gif ${outputPath}`);
+
+      const ffmpegGifArgs = [
+        '-hide_banner',
+        '-i', outputVideoPath,
+        '-pix_fmt', 'rgb24',
+        '-loop', '0',
+        outputPath,
+      ];
+    
+      const ffmpegGif = spawn(ffmpegPath, ffmpegGifArgs);
+    
+      ffmpegGif.on('error', (err) => {
+        node.warn(err);
+      });
+    
+      ffmpegGif.on('exit', (code) => {
+        if (code === 0) {
+          resolve();
+        } else {
+          reject(`ffmpegGif exited with code ${code}`);
+        }
+      });
+    
+      if (false) {
+        ffmpegGif.stdout.on('data', (data) => {
+          node.warn(`${data}`);
+        });
+    
+        ffmpegGif.stderr.on('data', (data) => {
+          node.warn(`${data}`);
+        });
+      }
+    } else {
+      reject(`ffmpeg exited with code ${code}`);
+    }
   });
 });
 
